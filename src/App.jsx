6121fc787a85ad58f9779791b31de3e7fa878518
lucas-bernardo-souza <lucasbernardodesouza510@gmail.com>
2025-12-29 13:41:01 -0300

    Meu primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css'
+import BarraDePesquisa from './components/BarraDePesquisa'
+import Filtro from './components/Filtro'
+import Sidebar from './components/Sidebar'
+import Card from './components/Card'
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes')
+    .then(resposta => resposta.json())
+    .then(dados => setDados(dados))
+  }, [])
+
+  return (
+    <div className='container'>
+      <Sidebar/>
+      <div>
+        <BarraDePesquisa/>
+        <Filtro/>
+        <ul className='lista-cards'>
+          {dados ? dados.map((item, index) =>(
+            <li key={index}>
+              <Card
+                id={item.id}
+                imagemUrl={item.imagem_capa}
+                titulo={item.titulo}
+                resumo={item.resumo}
+                linhasDeCodigo={item.linhas_de_codigo}
+                compartilhamentos={item.compartilhamentos}
+                comentarios={item.comentarios}
+                usuario={item.usuario}
+              />
+            </li>
+          )) : null}
+        </ul>
+        
+      </div>
+      
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,43 @@
+import Logo from './assets/logo.svg';
+import Feed from './assets/feed.svg';
+import Perfil from './assets/account_circle.svg';
+import Sobre from './assets/info.svg';
+import Sair from './assets/logout.svg';
+import './styles.css';
+
+export default function Sidebar(){
+    return(
+        <aside>
+            <img src={Logo} alt='Logo do CodeConnect' />
+            <nav>
+                <ul className='lista-sidebar'>
+                    <li><a href='#' className='item__link-publicacao'>Publicar</a></li>
+                    <li>
+                        <a href='#' className='item__link item__link--ativo'>
+                            <img src={Feed} alt=''/>
+                            <span>Feed</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' className='item__link'>
+                            <img src={Perfil} alt=''/>
+                            <span>Perfil</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' className='item__link'>
+                            <img src={Sobre} alt='' />
+                            <span>Sobre nós</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' className='item__link'>
+                            <img src={Sair} alt='' />
+                            <span>Sair</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
